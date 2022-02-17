b160ee47889acd9d9bcae9f114b7d945c637d699
Phillip Novess <phillip@novess.com>
2022-02-16 21:28:29 -0600

    revert to the basics

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,9 +7,18 @@ import Auth0Provider from "next-auth/providers/auth0"
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
+import jose from 'jose'
+// import prisma from '@/lib/prisma'
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     /* EmailProvider({
@@ -51,13 +60,90 @@ export default NextAuth({
       issuer: process.env.AUTH0_ISSUER,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+
+  jwt: {
+    // maxAge: 24 * 60 * 60,
+    // secret: process.env.NEXTAUTH_SECRET,
+    // signingKey: process.env.SIGNING_KEY,
+
+    // add claims hasura gql needs in the jwt
+    //   // const jwtClaims = {
+    //   //   "sub": token.id,
+    //   //   "name": token.name ,
+    //   //   "email": token.email,
+    //   //   "iat": Date.now() / 1000,
+    //   //   "exp": Math.floor(Date.now() / 1000) + (24*60*60),
+      //   //   "https://hasura.io/jwt/claims": {
+      //   //     "x-hasura-allowed-roles": ["user"],
+      //   //     "x-hasura-default-role": "user",
+      //   //     "x-hasura-role": "user",
+      //   //     "raw": token,
+      //   //     "x-hasura-user-id": token.id
+      //   //   }
+    //   // };
+
+    // async encode({ token, secret, signingKey }) {
+    //   const _signingKey = jose.JWK.asKey(JSON.parse(signingKey));
+    //   const signedToken = jose.JWT.sign(token, _signingKey, { algorithm: 'HS512' });
+    //   return signedToken;
+    // },
+
+    // async decode({ token, secret, signingKey }) {
+    //   // console.log(token)
+    //   // const tk = "eyJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoiUGhpbGxpcCBOb3Zlc3MiLCJlbWFpbCI6InBoaWxsaXBAbm92ZXNzLmNvbSIsInBpY3R1cmUiOiJodHRwczovL2F2YXRhcnMuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3UvOTAxMTQ_dj00Iiwic3ViIjoiY2t6cDgwMXdlMDA3NHAweHEzN3N3aGY1aSIsImlkIjoiY2t6cDgwMXdlMDA3NHAweHEzN3N3aGY1aSIsInVzZXIiOnsiaWQiOiJja3pwODAxd2UwMDc0cDB4cTM3c3doZjVpIiwibmFtZSI6IlBoaWxsaXAgTm92ZXNzIiwiZW1haWwiOiJwaGlsbGlwQG5vdmVzcy5jb20iLCJlbWFpbFZlcmlmaWVkIjpudWxsLCJpbWFnZSI6Imh0dHBzOi8vYXZhdGFycy5naXRodWJ1c2VyY29udGVudC5jb20vdS85MDExND92PTQifSwiaWF0IjoxNjQ0OTk3Njc1fQ.bMiyF-FZiGtBOhwtMMMCwxThkEwOwkO5nSFpJPrP0Vj5rmdvAtmj22ofMXcO4U60bvlQlr8vwb8_D3K5TiyR9w";
+    //   const _signingKey = jose.JWK.asKey(JSON.parse(signingKey));
+    //   const decodedToken = jose.JWT.verify(token, _signingKey, { algorithm: 'HS512' });
+    //   console.log( "DECODE", decodedToken);
+    //   return decodedToken;
+    // },
+  },
   theme: {
     colorScheme: "light",
   },
+  debug: false,  
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
+    // async jwt({ token }) {
+    //   token.userRole = "admin"
+    //   return token
+    // },
+
+    // modify to return userid for hasura gql
+    async session({session, token, user}) {
+      const _signingKey = jose.JWK.asKey(JSON.parse(process.env.SIGNING_KEY));
+      const signedToken = jose.JWT.sign(token, _signingKey, { algorithm: 'HS512' });
+      // console.log(user);
+      //const encodedToken = jose.JWT.sign(token, process.env.SECRET, { algorithm: 'HS512'});
+      // session.id = user.id;
+      session.accessToken = signedToken;
+      console.log( "SESSION", session);
+      return session;
     },
+
+    async jwt({token, user, account, profile, isNewUser}) {
+      const isUserSignedIn = user ? true : false;
+      // make a http call to our graphql api
+      // store this in postgres
+      // console.log(token)
+      if(isUserSignedIn) {
+        token.id = user.id.toString();
+        token.user = user;
+        token.userRole = "admin"
+
+        token.hasura = {
+          claims: {
+            "x-hasura-allowed-roles": ["user"],
+            "x-hasura-default-role": "user",
+            "x-hasura-role": "user",
+            "x-hasura-user-id": token.id
+          }
+        } 
+      } 
+
+      return token
+    }
+    
   },
 })
